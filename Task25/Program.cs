5c35970f1bf656841715cffa73ff88a308375311
helegra <e.b.g@mail.ru>
2022-11-25 19:26:52 +0300

    commit all

---
+++ Task25/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 25: Напишите цикл, 
+// который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// 1 Вариант:
+
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Итоговый результат = " + Math.Pow(num1, num2));
+
+// 2 Вариант:
+Console.WriteLine("Введите первое число, которое мы возводим в степень");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число - степень в которую мы возводим число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+double GetPowNum(int num, int pow)
+{
+    int p;
+
+    if (pow < 0)
+    {
+        p = -pow;
+    }
+    else
+    {
+        p = pow;
+    }
+    
+    int i;              // вводим счетчик
+    double result;         // результат расчета
+    result = 1;
+
+    for (i = 1; i <= p; i++)
+    {
+        result *= num;
+    }
+    if (pow < 0)
+    {
+        return 1 / result;
+    }
+    else
+    {
+        return result;
+    }
+}
+Console.WriteLine("Итоговый результат = " + GetPowNum(num1, num2));
---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, 
+// которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int num = new Random().Next();
+
+Console.WriteLine("Пусть некое число = " + num);
+
+int GetCountNum(int num)
+{
+    int result;
+    int Sum = 0;
+
+    while (num > 0)
+    {
+        result = num % 10;
+        Sum += result;
+        num /= 10;
+    }
+    return Sum;
+}
+Console.WriteLine("Тогда сумма цифр этого числа = " + GetCountNum(num));
